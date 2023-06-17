eb0e4687d3573848c016f65650c4d02267bd678c
djawed <djawed@gmail.com>
2023-06-17 13:54:25 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Your code here
+    console.log("dddddddddddddddd");
+    document.getElementById("demo").innerHTML = "Hello JavaScript!";
+});
+console.log("ssssssssss");
